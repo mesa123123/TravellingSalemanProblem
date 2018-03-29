148e229e0c8e455026a2e647d8767850e7a106bc
mesa123123 <bowmanpete123@gmail.com>
2018-03-29 22:52:43 +1300

    Added the ability to make a GA to solve the problem; also, refactoring

--- GA_TSP/Population.py
+++ GA_TSP/Population.py
@@ -1,4 +1,5 @@
-from Tools.Random_Permutation import random_permutation
+from Tools.Route import Route
+
 
 class Population:
     genes = None
@@ -6,6 +7,7 @@ class Population:
     def __init__(self, gene_number, city_number):
         cities_model = [i for i in range(2, city_number+1)]
         blank_genes = [[0]*1 for i in range(0, gene_number)]
+        print(blank_genes)
         for gene in blank_genes:
-            gene.append(list(random_permutation(cities_model)))
+            gene.append(Route(cities_model, False))
         self.genes = blank_genes
--- Tools/Route.py
+++ Tools/Route.py
@@ -4,7 +4,7 @@ from Tools.Random_Permutation import random_permutation
 class Route:
     route = None
 
-    def __init__(self, paver, cities, fix_start):
+    def __init__(self, cities, fix_start, paver = None):
 
         if not fix_start:
             self.route = list(random_permutation(cities))
