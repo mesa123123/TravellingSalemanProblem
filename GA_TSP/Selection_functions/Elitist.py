188c1231264cd4d0736846a5700ba73d8706ba77
mesa123123 <bowmanpete123@gmail.com>
2018-04-01 01:22:07 +1300

    Worked on building functions for cross_over, and selection_functions for the implementation of the GA

--- GA_TSP/Selection_functions/Elitist.py
+++ GA_TSP/Selection_functions/Elitist.py
@@ -1,10 +1,23 @@
 from GA_TSP.Score_Pop import score_pop
 from GA_TSP.Crossover_functions.Path_Encoding import partially_mapped_crossover
 
+
 def queen_bee(Population, roads):
     if not Population.scored:
         score_pop(Population, roads)
     queen_bee = Population.genes[0][1]
     for i in range(1, len(Population.genes)):
-        Population[i][1] = partially_mapped_crossover(queen_bee, Population.genes[i][1], True)
+        Population[i] = [0, partially_mapped_crossover(queen_bee, Population.genes[i][1], True)]
 
+
+def elite_nuclear_family(Population, roads):
+    if not Population.scored:
+        score_pop(Population, roads)
+    for i in range(0, len(Population.genes), 2):
+        children = partially_mapped_crossover(Population[i][1], Population[i+1][1])
+        Population[i] = [0, children[0]]
+        Population[i+1] = [0, children[1]]
+
+
+def probabilistic_selection(Population, roads):
+    pass
