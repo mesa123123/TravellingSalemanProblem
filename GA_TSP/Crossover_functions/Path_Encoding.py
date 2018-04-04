a34ea3b628038fe975e150101c9add2d6c3a19ea
mesa123123 <bowmanpete123@gmail.com>
2018-04-04 18:04:16 +1200

    Worked on building functions for cross_over, and selection_functions for the implementation of the GA

--- GA_TSP/Crossover_functions/Path_Encoding.py
+++ GA_TSP/Crossover_functions/Path_Encoding.py
@@ -1,6 +1,55 @@
+from random import randint
+
 
 def partially_mapped_crossover(p1, p2, only_child = False):
-    pass
+    #cuts the parent genomes and switches the genes within the cuts
+    first_cut = randint(0,len(p1)//2 - 1)
+    second_cut = randint(len(p1)//2,len(p1))
+    cut_genome = random_cut(p1, p2, first_cut, second_cut)
+    #Work out any genes that have doubled up, in this context stop the children from visiting cities more than once
+    c1 = [cut_genome[0][i] for i in range(0,len(cut_genome[0]))]
+    c2 = [cut_genome[1][i] for i in range(0, len(cut_genome[1]))]
+    #switch the genes outside the cuts for C1
+    rearrange = True
+    i = first_cut
+    j = 0
+    while(rearrange):
+        if c1[i] == c1[j]:
+            c1[j] = c2[i]
+            i = first_cut
+            j = 0
+        else:
+            j += 1
+        if j == first_cut:
+            j = second_cut
+        if j == len(p1):
+            i += 1
+            j = 0
+        if i == second_cut:
+            rearrange = False
+    #iron out kinks in C2
+    i = first_cut
+    j = 0
+    rearrange = True
+    while (rearrange):
+        if c2[i] == c2[j]:
+            c2[j] = c1[i]
+            i = first_cut
+            j = 0
+        else:
+            j += 1
+        if j == first_cut:
+            j = second_cut
+        if j == len(p1):
+            i += 1
+            j = 0
+        if i == second_cut:
+            rearrange = False
+    #If we want the parents to create one or two child genomes
+    if only_child:
+        return [0, c1]
+    else:
+        return [[0, c1],[0, c2]]
 
 
 def order_crossover():
@@ -9,3 +58,25 @@ def order_crossover():
 
 def cycle_crossover():
     pass
+
+#function that allows the genes within the cut to switch over in their children
+def random_cut(p1, p2, first_cut, second_cut):
+    c1 = [0]*len(p1)
+    c2 = [0]*len(p2)
+    #Populate the children genomes through the crossover at two arbitrary points; perhaps we could arbitrary an
+    # un-artbitrary these points?
+    for i in range(first_cut, second_cut):
+        c1[i] = p2[i]
+        c2[i] = p1[i]
+    for i in range(0,first_cut):
+        c1[i] = p1[i]
+        c2[i]= p2[i]
+    for i in range(second_cut,len(p1)):
+        c1[i] = p1[i]
+        c2[i] = p2[i]
+    return [c1, c2]
+
+p1 = [1,3,2,6,4,5,7,9,8]
+p2 = [2,4,5,8,7,6,9,1,3]
+
+partially_mapped_crossover(p1,p2,True)
--- GA_TSP/Population.py
+++ GA_TSP/Population.py
@@ -1,6 +1,5 @@
 from Tools.Random_Permutation import random_permutation
-from Tools.Problem_Matrix import ProblemMatrix
-from GA_TSP.Score_Pop import score_pop
+
 
 class Population:
     genes = None
@@ -8,9 +7,9 @@ class Population:
     roads = None
 
     def __init__(self, gene_number, city_number):
-         cities_model = [i for i in range(2, city_number+1)]
-         blank_genes = [[0]*2 for i in range(0, gene_number)]
-         for gene in blank_genes:
-             gene[1] = random_permutation(cities_model)
-         self.genes = blank_genes
-         self.scored = False
+        cities_model = [i for i in range(2, city_number+1)]
+        blank_genes = [[0]*2 for i in range(0, gene_number)]
+        for gene in blank_genes:
+            gene[1] = random_permutation(cities_model)
+        self.genes = blank_genes
+        self.scored = False
