73658e518119ce9126c393e84bf8d7ac1501a01f
mesa123123 <bowmanpete123@gmail.com>
2018-04-17 21:25:04 +1200

    Mutation Algorithms added

--- GA_TSP/Mutations.py
+++ GA_TSP/Mutations.py
@@ -1,21 +1,34 @@
 import random as rnd
 
 def swap_mutation(gene, mutation_chance):
-     mutate = rnd.random()
-     if mutate < mutation_chance:
+    mutate = rnd.random()
+    if mutate < mutation_chance:
+        cut1 = rnd.randint(0,len(gene)-2)
+        cut2 = rnd.randint(cut1, len(gene)-1)
+        temp = gene[cut2]
+        gene[cut2] = gene[cut1]
+        gene[cut1] = temp
+    return gene
 
 
-def insertion_mutation:
+def insertion_mutation(gene, mutation_chance):
+    mutate = rnd.random()
+    if mutate < mutation_chance:
+        cut1 = rnd.randint(0, len(gene)-3)
+        cut2 = rnd.randint(cut1 + 1, len(gene)-1)
+        temp = gene[cut2]
+        for i in range(cut2, cut1+1, -1):
+            gene[i+1] = gene[i]
+        gene[cut1+1] = temp
+    return gene
+
+def displacement_mutation():
     pass
 
 
-def displacement_mutation:
+def scramble_mutation():
     pass
 
 
-def scramble_mutation:
-    pass
-
-
-def inversion_mutation:
+def inversion_mutation():
     pass
