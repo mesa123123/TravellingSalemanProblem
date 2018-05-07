59376234e28ed699f98e669c79e53b1f821c4436
mesa123123 <bowmanpete123@gmail.com>
2018-05-07 14:44:49 +1200

    Works well, though is a bloody mess and is in dire need of a UI!

--- Supporting_Tools/Solutions.py
+++ Supporting_Tools/Solutions.py
@@ -4,10 +4,12 @@ from Genetic_Algorithm_Files.Evolve_Population import *
 from Genetic_Algorithm_Files.Population import Population
 from Supporting_Tools.Random_Permutation import random_permutation
 from Supporting_Tools import Create_Dirs
+from Supporting_Tools import Route
+from TSP.Climbs import steep_climb
 
 
 # The variables that control the outputs #
-run_total = 10
+run_total = 1
 # Sets up a list of the number of the cities
 number_of_cities = 50
 cities = [x for x in range(2, number_of_cities+1)]
@@ -24,7 +26,7 @@ First_Genes = [i for i in Current_Generation.genes]
 # Decides which genetic controls will be used #
 # ------------------------------------------- #
 # Valid mutations are {swap, invert, displace, scramble, insert, None}
-mutation = "displace"
+mutation = "invert"
 # Valid Parent Algorithms are {rank, random}
 parent_selection = "rank"
 # Valid Crossover Algorithms are {partially mapped, order, cycle}
@@ -47,7 +49,8 @@ average_graph_title = "Average of " + crossover + " crossover function with " + 
 worst_journeys = list()
 worst_graph_title = "Worst of " + crossover + " crossover function with " + mutation + " mutation function"
 # Creates a directory for the outputted files
-dir_title = str(selection) + " " +  str(parent_selection) + " " + str(crossover) + " " + str(mutation)
+#dir_title = str(selection) + " " +  str(parent_selection) + " " + str(crossover) + " " + str(mutation)
+dir_title = "Local Search"
 Create_Dirs.main(dir_title)
 for i in range(1, run_total + 1):
     title = "Run: " + str(i)
@@ -57,21 +60,24 @@ for i in range(1, run_total + 1):
     output_title = "../Results/" + dir_title + "/Full Scores Of Population/Population Scores.Run " \
                    + str(i) + ".txt"
     output_file = open(output_title, 'w')
-    results = create_result(Current_Generation, paver, generation, crossover, parent_selection,
-                                   selection, mutation, mutation_chance, output_file)
-    best_journeys.append([results[0], title])
-    worst_journeys.append([results[1], title])
-    average_journeys.append([results[2], title])
+    Climb_Route = Route.Route(cities, True, paver)
+    # results = create_result(Current_Generation, paver, generation, crossover, parent_selection,
+    #                               selection, mutation, mutation_chance, output_file)
+    #
+    # best_journeys.append([results[0], title])
+    # worst_journeys.append([results[1], title])
+    # average_journeys.append([results[2], title])
+    output_file.write(str(steep_climb(20,Climb_Route.route,roads)))
     output_file.close()
     # Reset the genes back to their original set
-    Current_Generation.genes = [i for i in First_Genes]
-# Plots Results of the Run Algorithm to a graph #
-plot_travels(best_journeys, best_graph_title, dir_title)
-plot_travels(worst_journeys, worst_graph_title, dir_title)
-plot_travels(average_journeys, average_graph_title, dir_title)
-# Plots the average, worst and best population scores for each generation on the graph
-for i in range(0, run_total):
-    plot_travels([best_journeys[i], worst_journeys[i], average_journeys[i]], "Run " + str(i + 1) + ", all stats",
-                 dir_title)
-
+#     Current_Generation.genes = [i for i in First_Genes]
+# # Plots Results of the Run Algorithm to a graph #
+# plot_travels(best_journeys, best_graph_title, dir_title)
+# plot_travels(worst_journeys, worst_graph_title, dir_title)
+# plot_travels(average_journeys, average_graph_title, dir_title)
+# # Plots the average, worst and best population scores for each generation on the graph
+# for i in range(0, run_total):
+#     plot_travels([best_journeys[i], worst_journeys[i], average_journeys[i]], "Run " + str(i + 1) + ", all stats",
+#                  dir_title)
+#
 
