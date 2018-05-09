847eaa02a2697710a44611add60a029bffe060f2
mesa123123 <bowmanpete123@gmail.com>
2018-05-10 11:00:21 +1200

    Works well, though is a bloody mess and is in dire need of a UI!

--- Supporting_Tools/Solutions.py
+++ Supporting_Tools/Solutions.py
@@ -9,13 +9,16 @@ from TSP.Climbs import steep_climb
 
 
 # The variables that control the outputs #
-run_total = 1
+run_total = 50
 # Sets up a list of the number of the cities
 number_of_cities = 50
 cities = [x for x in range(2, number_of_cities+1)]
 # Variables that control the population
 pop_size = 16
-generation = number_of_cities*20
+generation = 10000
+# Tracks the amount of completed runs on the algorithm
+count = 1
+
 # Mutation Constant, the bigger this is the less likely mutation is to occur
 mutation_constant = 100
 # Creates initial Population
@@ -30,7 +33,7 @@ mutation = "invert"
 # Valid Parent Algorithms are {rank, random}
 parent_selection = "rank"
 # Valid Crossover Algorithms are {partially mapped, order, cycle}
-crossover = "order"
+crossover = "cycle"
 # Valid Selection Algorithms are {elitism, on_fitness}
 selection = "elitism"
 # set up the roads between the cities
@@ -60,24 +63,26 @@ for i in range(1, run_total + 1):
     output_title = "../Results/" + dir_title + "/Full Scores Of Population/Population Scores.Run " \
                    + str(i) + ".txt"
     output_file = open(output_title, 'w')
-    Climb_Route = Route.Route(cities, True, paver)
-    # results = create_result(Current_Generation, paver, generation, crossover, parent_selection,
-    #                               selection, mutation, mutation_chance, output_file)
-    #
-    # best_journeys.append([results[0], title])
-    # worst_journeys.append([results[1], title])
-    # average_journeys.append([results[2], title])
-    output_file.write(str(steep_climb(20,Climb_Route.route,roads)))
+    results = create_result(Current_Generation, paver, generation, crossover, parent_selection,
+                                  selection, mutation, mutation_chance, output_file)
+
+    best_journeys.append([results[0], title])
+    worst_journeys.append([results[1], title])
+    average_journeys.append([results[2], title])
     output_file.close()
+    # Displays the completed runs of the algorithm so the user doesn't get impatient
+    print("completed \t" + str(count))
+    count += 1
     # Reset the genes back to their original set
-#     Current_Generation.genes = [i for i in First_Genes]
-# # Plots Results of the Run Algorithm to a graph #
-# plot_travels(best_journeys, best_graph_title, dir_title)
-# plot_travels(worst_journeys, worst_graph_title, dir_title)
-# plot_travels(average_journeys, average_graph_title, dir_title)
-# # Plots the average, worst and best population scores for each generation on the graph
-# for i in range(0, run_total):
-#     plot_travels([best_journeys[i], worst_journeys[i], average_journeys[i]], "Run " + str(i + 1) + ", all stats",
-#                  dir_title)
-#
+    Current_Generation.genes = [i for i in First_Genes]
+# Plots Results of the Run Algorithm to a graph #
+plot_travels(best_journeys, best_graph_title, dir_title)
+plot_travels(worst_journeys, worst_graph_title, dir_title)
+plot_travels(average_journeys, average_graph_title, dir_title)
+# Displays the completed runs of the algorithm so the user doesn't get impatient
+# Plots the average, worst and best population scores for each generation on the graph
+for i in range(0, run_total):
+    plot_travels([best_journeys[i], worst_journeys[i], average_journeys[i]], "Run " + str(i + 1) + ", all stats",
+                 dir_title)
+
 
