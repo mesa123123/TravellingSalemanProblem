32e0fa36b7a69cd59d55d4b8d897a24f3b848935
Peter Bowman <bowmanpete123@gmail.com>
2025-09-29 00:17:19 +0100

    Cleaned up Logistics Utils

--- src/logistics/utils.py
+++ src/logistics/utils.py
@@ -17,34 +17,44 @@ def create_road_network(num_cities: int, road_length_limit: float) -> RoadNetwor
     return route_network
 
 
-def plot_route(route: Optional[Route], all_roads: RoadNetwork) -> Route:
-    if not route:
-        rnd = default_rng()
-        rnd_route = rnd.permutation(len(all_roads) + 1)
-        destinations = [Destination(visit_number=i, current_city=rnd_route[i]) for i in range(len(all_roads))]
+def create_route(road_network: RoadNetwork) -> Route:
+    rnd = default_rng()
+    rnd_route = rnd.permutation(len(road_network) + 1)
+    destinations = [Destination(visit_number=i, current_city=rnd_route[i]) for i in range(len(road_network))]
+    return Route(route_plot=destinations, route_score=-0.0)
 
-        route = Route(route_plot=destinations, route_score=-0.0)
-    destinations: list[Destination] = sorted(route.route_plot, key=lambda d: d.visit_number)
+
+def _score_distances(destinations: list[Destination], road_network: RoadNetwork) -> list[Destination]:
     destinations[0].arrived_by_road = None
     for i in range(1, len(destinations)):
-        city_B = destinations[i].current_city
-        city_A = destinations[i - 1].current_city
+        city_B: int = destinations[i].current_city
+        city_A: int = destinations[i - 1].current_city
         connecting_road: Optional[Road] = next(
-            (road for road in all_roads if {road.departure_city, road.arrival_city} == {city_A, city_B}),
+            (road for road in road_network if {road.departure_city, road.arrival_city} == {city_A, city_B}),
             None,
         )
         if not connecting_road:
             raise ValueError(f"There is a missing road in the route_network between city: {city_A} and city: {city_B}")
         destinations[i].arrived_by_road = connecting_road
+    return destinations
+
+
+def plot_route(road_network: RoadNetwork, route: Optional[Route] = None) -> Route:
+    route: Route = route if route else create_route(road_network)
+    destinations: list[Destination] = sorted(route.route_plot, key=lambda d: d.visit_number)
+    destinations: list[Destination] = _score_distances(destinations, road_network)
     total_dist: float = sum([dest.arrived_by_road.road_length for dest in destinations])
     return Route(destinations, total_dist)
 
 
-def score_route_population(population: RoutePopulation, road_network: RoadNetwork) -> RoutePopulation:
-    scored_route_population: RoutePopulation = [plot_route(route, road_network) for route in population.routes]
-
-    scored_route_population.routes = sorted(scored_route_population.routes, key=lambda x: x.route_score)
-    return scored_route_population
+def create_route_population(population_size: int, road_network: RoadNetwork) -> RoutePopulation:
+    pop: RoutePopulation = RoutePopulation(
+        population_size=population_size,
+        num_of_cities=len(road_network),
+        routes=[plot_route(road_network) for i in range(population_size)],
+    )
+    pop.routes = sorted(pop.routes, key=lambda x: x.route_score)
+    return pop
 
 
 def get_best_swapped_route(route, roads) -> Route:
