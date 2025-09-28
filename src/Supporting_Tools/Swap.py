c7a8c47ef55164a4d00745f7296f49665af536db
Peter Bowman <bowmanpete123@gmail.com>
2025-09-28 20:23:16 +0100

    Updated the swap functions to be more concise and added dataclasses (mostly for my sanity)

--- src/Supporting_Tools/Swap.py
+++ src/Supporting_Tools/Swap.py
@@ -1,29 +1,27 @@
-import Supporting_Tools.Distance_check as Dcheck
+from dataclasses import dataclass
+
+from src.supporting_tools.distance_check import distance_check
 
 
-def swap_member(route, member):
-    temp = route[member]
-    route[member] = route[member + 1]
-    route[member + 1] = temp
-    return route
+@dataclass
+class Route:
+    roads: list[int]
+    distances: list[int]
 
 
 def best_distance(route, roads):
     alt_routes = [[] for i in range(1, int(len(roads)) - 2)]
-    swap_member = 0
-    # Finds all possible switches and calculates them by distannce
-    while swap_member < int(len(roads)) - 3:
-        temp_stats = next_distance(route, swap_member, roads)
-        alt_routes[swap_member].append(temp_stats[0])
-        alt_routes[swap_member].append(temp_stats[1])
+    while swap_member := 0 < int(len(roads)) - 3:
+        new_route = next_distance(route, swap_member, roads)
+        alt_routes[swap_member].append(new_route.roads)
+        alt_routes[swap_member].append(new_route.distances)
         swap_member += 1
     # sort the alternate by the distances
     alt_routes = sorted(alt_routes, key=lambda x: x[1])
     return alt_routes[0]
 
 
-def next_distance(route, member, roads):
-    stats = []
-    stats.append(swap_member([i for i in route], member))
-    stats.append(Dcheck.distance_check(stats[0], roads))
-    return stats
+def next_distance(routes: list[int], member: int, roads) -> Route:
+    route_copy: list[int] = routes.copy()
+    route_copy[member], route_copy[member + 1] = route_copy[member + 1], route_copy[member]
+    return Route(roads=route_copy, distances=distance_check(route_copy, roads))
